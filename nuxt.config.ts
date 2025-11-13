4386aec08b2a2e68fca1478b6eafce05641b47bf
Lauris Priede <lauris@wdmarket.lv>
2025-11-14 03:45:55 +0800

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
+import netlify from '@nuxtjs/netlify'
+
 export default defineNuxtConfig({
   extends: ['./woonuxt_base'],
   components: [{ path: './components', pathPrefix: false }],
   ssr: true,
+  modules: [netlify()],
   nitro: {
     preset: 'netlify', // 👈 enables Netlify SSR build
   },
